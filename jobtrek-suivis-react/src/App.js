c101a3790ed72f92e08e5f4243b0d90cd80b3689
nicolasloison13 <nicolasloison13@icloud.com>
2023-05-26 11:26:02 +0200

    fix probleme with merge

--- jobtrek-suivis-react/src/App.js
+++ jobtrek-suivis-react/src/App.js
@@ -1,38 +1,20 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
 import { createTheme } from './theme'
 import { CssBaseline, ThemeProvider} from "@mui/material";
 import Topbar from "./pages/global/Topbar";
-import UserList from "./components/UserList";
 
->>>>>>> Stashed changes
-=======
-import React from 'react';
-import UserList from './components/UserList';
-
->>>>>>> frontend
 function App() {
 
     const theme = createTheme();
 
   return (
-<<<<<<< Updated upstream
-    <div className="app">
-      <h1>mon app</h1>
-      <UserList />
-    </div>
-=======
         <ThemeProvider theme={theme}>
             <CssBaseline/>
             <div className="app">
                 <main className="content">
                     <Topbar />
-                    <UserList></UserList>
                 </main>
             </div>
         </ThemeProvider>
->>>>>>> Stashed changes
   );
 }
 
