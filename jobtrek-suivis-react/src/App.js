781d97e1c3d39f991e1d0708a0ac89568413d80b
nicolasloison13 <nicolasloison13@icloud.com>
2023-05-26 11:15:01 +0200

    some try with fetch data

--- jobtrek-suivis-react/src/App.js
+++ jobtrek-suivis-react/src/App.js
@@ -1,7 +1,30 @@
+<<<<<<< Updated upstream
+=======
+import { createTheme } from './theme'
+import { CssBaseline, ThemeProvider} from "@mui/material";
+import Topbar from "./pages/global/Topbar";
+import UserList from "./components/UserList";
+
+>>>>>>> Stashed changes
 function App() {
+
+    const theme = createTheme();
+
   return (
+<<<<<<< Updated upstream
     <div className="app">
     </div>
+=======
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <div className="app">
+                <main className="content">
+                    <Topbar />
+                    <UserList></UserList>
+                </main>
+            </div>
+        </ThemeProvider>
+>>>>>>> Stashed changes
   );
 }
 
