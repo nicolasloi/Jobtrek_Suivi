0154485e5408a720b0686387d403fc4817f53c7d
nicolasloison13 <nicolasloison13@icloud.com>
2023-05-30 09:16:10 +0200

    create Header

--- jobtrek-suivis-react/src/pages/dashboard/index.jsx
+++ jobtrek-suivis-react/src/pages/dashboard/index.jsx
@@ -1,6 +1,12 @@
+import Header from "../../components/Header";
+import {Box} from "@mui/material";
 
 const Dashboard = () => {
-    return <div>Dashboard</div>
+    return <Box m="20px">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Header title="DASHBOARD" subtitle="Bienvenue sur votre dashboard"/>
+        </Box>
+    </Box>;
 }
 
 export default Dashboard;
